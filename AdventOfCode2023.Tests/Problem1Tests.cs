f213b42db3672f7451523058a4ebb6faaad5b28e
Adam Friswell <adamfriswell@googlemail.com>
2023-12-13 22:36:39 +0000

    add unit test for sum, reduce lines of code

--- AdventOfCode2023.Tests/Problem1Tests.cs
+++ AdventOfCode2023.Tests/Problem1Tests.cs
@@ -9,9 +9,16 @@ public class Problem1Tests
     [InlineData("pqr3stu8vwx",38)]
     [InlineData("a1b2c3d4e5f",15)]
     [InlineData("treb7uchet",77)]
-    public void GivenTestInputs_GetCalibrationValue_ShouldGetExpectedAnswer(string input, int expectedOutput)
+    public void GivenTestInputs_GetCalibrationValue_ShouldGetExpectedAnswer(string input, int expectedValue)
     {
         var result = Problem1.GetCalibrationValue(input);
-        Assert.Equal(result, expectedOutput);
+        Assert.Equal(expectedValue, result);
+    }
+
+    [Fact]
+    public void GivenTestInputs_GetCalibrationSum_ShouldGetExpectedAnswer()
+    {
+        var result = Problem1.GetCalibrationSum(new() { "1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet" });
+        Assert.Equal(142, result);
     }
 }
--- AdventOfCode2023/Problems/Problem1.cs
+++ AdventOfCode2023/Problems/Problem1.cs
@@ -5,27 +5,15 @@
         public static int Solve()
         {
             var input = File.ReadLines(Path.Combine(Directory.GetCurrentDirectory(), "Inputs", "Problem1Input.txt")).ToList();
-
-            var sum = 0;
-            foreach (var line in input)
-            {
-                var calibrationValue = GetCalibrationValue(line);
-                sum += calibrationValue;
-            }
-
-            return sum;
+            return GetCalibrationSum(input);
         }
+
+        public static int GetCalibrationSum(List<string> input) => input.Sum(x => GetCalibrationValue(x));
 
         public static int GetCalibrationValue(string inputLine)
         {
-            List<int> digitsOnly = new(){ };
-            foreach (var c in inputLine.ToCharArray())
-            {
-                if (char.IsDigit(c))
-                {
-                    digitsOnly.Add(Convert.ToInt32(c.ToString()));
-                }
-            }
+            var digitsOnly = inputLine.Where(x => char.IsDigit(x))
+                                      .Select(x => Convert.ToInt32(x.ToString()));
 
             return Convert.ToInt32(string.Format("{0}{1}", digitsOnly.First(), digitsOnly.Last()));
         }
